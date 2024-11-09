69c3bd95b3f923da8c3c66d99ff1d578a6d81168
ashwinj <ashwin.jandhyala@gmail.com>
2024-11-09 15:07:58 -0600

    added phone numbers

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ from sheets import *
 
 # Define search parameters
 address = 'Madison Wisconsin, USA' # where to search
-search_string = 'fast food' # what type of business to search for
+search_string = 'local restraunts' # what type of business to search for
 radius_miles = 1 # will search in an x mile radius around the address
-fields = ['place_id', 'name', 'formatted_address', 'rating', 'user_ratings_total', 'website'] # will put the following data fields into the spreadsheet
+fields = ['place_id', 'name', 'formatted_address', 'rating', 'user_ratings_total', 'website', "formatted_phone_number"] # will put the following data fields into the spreadsheet
 
 # Search for businesses based on parameters
 business_list = search_businesses(address, search_string, radius_miles)
@@ -16,7 +16,11 @@ business_list = search_businesses(address, search_string, radius_miles)
 businesses_info = get_businesses_info(business_list, fields)
 
 # Retrieve email addresses for businesses
-businesses_list_with_emails = get_business_emails(businesses_info)
+# UNCOMMENT IF NEEDED
+# businesses_list_with_emails = get_business_emails(businesses_info)
 
-# Create and save the results to a dataframe
-create_and_save_dataframe(businesses_list_with_emails, search_string, address)
+# Create and save the results to a dataframe with emails
+# create_and_save_dataframe(businesses_list_with_emails, search_string, address)
+
+# Create and save the results to a dataframe without emails
+create_and_save_dataframe(businesses_info, search_string, address)
